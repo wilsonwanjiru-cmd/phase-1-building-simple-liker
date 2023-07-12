85ff4c8e985aa80b1a9d7beada8adabfa732404a
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-12 14:26:16 +0300

    <Building the Simple Liker App>

--- main.js
+++ main.js
@@ -3,6 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Get the error modal element
+const errorModal = document.getElementById('modal');
+
+// Function to toggle the heart state
+function toggleHeart(heart) {
+  if (heart.innerText === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.innerText = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch(() => {
+        errorModal.classList.remove('hidden');
+        errorModal.querySelector('#modal-message').innerText = 'Random server error. Try again.';
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  } else if (heart.innerText === FULL_HEART) {
+    heart.innerText = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
+// Attach click event listener to all hearts
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach((heart) => {
+  heart.addEventListener('click', () => {
+    toggleHeart(heart);
+  });
+});
+
+
+
 
 
 
